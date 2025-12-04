64bea18cb0c46aed77b1fed340c39019832e8744
Varshini Shekar <varshinishekar15@gmail.com>
2025-12-04 16:53:49 +0530

    Update helloworld.java

--- src/main/java/com/example/helloworld.java
+++ src/main/java/com/example/helloworld.java
@@ -2,5 +2,6 @@ package com.example;
 public class helloworld{
 public static void main (String[] args){
   System.out.println("hello, devops CI/CD using github actions");
+  System.out.println("verifying automatic workflow trigger"); 
 }
 }
